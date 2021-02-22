9c0407f2c5a9605b0cc0b5903445035528a44e3a
ThomasCorcoral <thomas.corcoral@edu.univ-fcomte.fr>
2021-02-22 15:53:04 +0100

    initial modif

--- Format.h
+++ Format.h
@@ -4,12 +4,33 @@
 #include <string>
 
 namespace fp {
-
   /**
    * @brief Insert all parameter into a formated string
    */
-  std::string format(const std::string& formatString /* implementation defined */) {
+  std::string format(const std::string& formatString){
     return formatString;
+  }
+
+  template<typename T, typename... Targs>
+  std::string format(const std::string& formatString, T value, Targs ... Fargs) {
+    std::string stringFormated;
+    int i = 0;
+    while(i < formatString.length()){
+      char c = formatString[i];
+
+      if(c == '%'){
+        if(formatString[i+1] == '%'){
+          stringFormated += formatString[i+1];
+          i++;
+        }
+      }else{
+        stringFormated += "True";
+        stringFormated += formatString.substr(i+2,formatString.length());
+        return format(stringFormated, Fargs...);
+      }
+
+    }
+    return stringFormated;
   }
 
 }
--- testFormatPrint.cc
+++ testFormatPrint.cc
@@ -2,6 +2,21 @@
 
 #include "Format.h"
 
+namespace my {
+  struct Foo {
+    int i;
+  };
+
+  std::string to_string(const Foo& foo) {
+    return std::to_string(foo.i);
+  }
+}
+
+TEST(EasyTest, SimpleNoChange){
+  auto str = fp::format("The cake is a lie!");
+  EXPECT_EQ(str, "The cake is a lie!");
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();

3ecf385a24bd24e38ff554695753cc381215f1f8
ThomasCorcoral <thomas.corcoral@edu.univ-fcomte.fr>
2021-02-22 16:20:46 +0100

    format update

--- Format.h
+++ Format.h
@@ -8,26 +8,72 @@ namespace fp {
    * @brief Insert all parameter into a formated string
    */
   std::string format(const std::string& formatString){
-    return formatString;
+    std::string stringFormated;
+    size_t i = 0;
+    while(i < formatString.length()){
+      char c = formatString[i];
+      if(c == '%'){
+        if(formatString[i+1] == '%'){
+          stringFormated += c;
+          i++;
+        }else{
+          // TODO : Throw exception because no more arguments
+          stringFormated += c; // To remove
+        }
+      }else{
+        stringFormated += c;
+      }
+      i++;
+    }
+    return stringFormated;
   }
 
   template<typename T, typename... Targs>
   std::string format(const std::string& formatString, T value, Targs ... Fargs) {
     std::string stringFormated;
-    int i = 0;
+    size_t i = 0;
     while(i < formatString.length()){
       char c = formatString[i];
 
       if(c == '%'){
-        if(formatString[i+1] == '%'){
-          stringFormated += formatString[i+1];
-          i++;
+        char type = formatString[i+1];
+        switch (type){
+          case 'i':
+          case 'd':
+            // TODO : Check the type typeid ?
+          case 'f':
+            // TODO : Check the type typeid ?
+            break;
+          case 'b':
+            // TODO : Check the type typeid ?
+            if(value){
+              stringFormated += "true";
+            }else{
+              stringFormated += "false";
+            }
+            stringFormated += formatString.substr(i+2,formatString.length());
+            return format(stringFormated, Fargs...);
+            break;
+          case 's':
+            // TODO : Check the type typeid ?
+            break;
+          case 'c':
+            // TODO : Check the type typeid ?
+            break;
+          case 'x':
+            // TODO : Check the type typeid ?
+            break;
+          case '%':
+            stringFormated += type;
+            stringFormated += formatString.substr(i+3,formatString.length());
+            return format(stringFormated, value, Fargs...);
+          default:
+            break;
         }
       }else{
-        stringFormated += "True";
-        stringFormated += formatString.substr(i+2,formatString.length());
-        return format(stringFormated, Fargs...);
+        stringFormated += c;
       }
+      i++;
 
     }
     return stringFormated;
